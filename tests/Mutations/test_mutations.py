3139619aff831eae2872fc3393fcd69b38cdb6fb
Poojar-git <pooja.r@diggibyte.com>
2026-07-09 15:46:59 +0530

    Add test cases for mutate_string function

--- tests/Mutations/test_mutations.py
+++ tests/Mutations/test_mutations.py
@@ -1,4 +1,7 @@
 from src.Mutations.utils import mutate_string
 def test1():
     assert mutate_string("apples", 4, "o")=="applos"
-
+def test1():
+    assert mutate_string("laptop", 1, "o")=="loptop"
+def test1():
+    assert mutate_string("main", 0, "o")=="oain"
